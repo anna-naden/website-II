cc198cb8b3858a5344b7d8cc4eef9f2c766a0375
Alan Turing <alan.turing@gmail.com>
2020-09-23 16:13:48 -0500

    try to fix templating. county still puts home button in wrong place

--- py/apply_template.py
+++ py/apply_template.py
@@ -4,19 +4,11 @@ file_loader = FileSystemLoader("../templates")
 env = Environment(loader=file_loader)
 template = env.get_template('us-hot2-t.html')
 
-try:
-    with open('../templates/google-analytics.html', 'r') as f:
-        ga = f.read()
-    with open('../templates/logo.html', 'r') as f:
-        logo = f.read()
-except:
-    print("error reading templates")
-    exit()
-output = template.render(google_analytics=ga, logo=logo)
+output = template.render()
 with open('../static/us-hot2.html','w') as f:
     f.write(output)
 
 template = env.get_template('county-hot-t.html')
-output = template.render(google_analytics=ga, logo=logo)
+output = template.render()
 with open('../static/county-hot.html', 'w') as f:
     f.write(output)
