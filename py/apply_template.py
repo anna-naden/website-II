5c12ebfe20d3425d96f0b14205c6348caedf48ec
Alan Turing <alan.turing@gmail.com>
2020-09-23 17:18:48 -0500

    got home button to follow map

--- py/apply_template.py
+++ py/apply_template.py
@@ -7,8 +7,3 @@ template = env.get_template('us-hot2-t.html')
 output = template.render()
 with open('../static/us-hot2.html','w') as f:
     f.write(output)
-
-template = env.get_template('county-hot-t.html')
-output = template.render()
-with open('../static/county-hot.html', 'w') as f:
-    f.write(output)
