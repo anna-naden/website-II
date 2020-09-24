95d7a8b2cf20662ef78d6def7d786cd8f9c93201
Alan Turing <alan.turing@gmail.com>
2020-09-23 20:38:43 -0500

    remove temp csv file

--- py/apply_template.py
+++ py/apply_template.py
@@ -1,9 +1,12 @@
 from jinja2 import Environment, FileSystemLoader
+from get_config import get_config
 
-file_loader = FileSystemLoader("../templates")
+config = get_config()
+file_loader = FileSystemLoader(config['DEFAULT']['templates'])
 env = Environment(loader=file_loader)
 template = env.get_template('us-hot2-t.html')
 
 output = template.render()
-with open('../static/us-hot2.html','w') as f:
+with open(config['FILES']['us_hot'],'w') as f:
     f.write(output)
+print('static html created')
