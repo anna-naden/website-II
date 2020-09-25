50691449f8f3de99725a10e4fd36991883ae61df
Alan Turing <alan.turing@gmail.com>
2020-09-25 18:00:25 -0500

    got lambda function working with api gateway and ipyleaflet, at least locally

--- py/make_county_features.py
+++ py/make_county_features.py
@@ -1,38 +1,35 @@
 import boto3
+import logging
+from botocore.exceptions import ClientError
+import os
+import sys
+import threading
+
 import numpy as np
 import json
 
 from county_pops_fips import county_pops_fips
 from get_world_covid_jh import get_world_covid_jh
 
-def stringify(num):
-    return f'{num}'
+class ProgressPercentage(object):
 
-def stringify_deep_poly(l):
-    out=[]
-    for c in l:
-        out1=[]
-        for c2 in c:
-            map1 = map(stringify,c2)
-            result = list(map1)
-            out1.append(result)
-        out.append(out1)
-    return out
+    def __init__(self, filename):
+        self._filename = filename
+        self._size = float(os.path.getsize(filename))
+        self._seen_so_far = 0
+        self._lock = threading.Lock()
 
-def stringify_deep_multi_poly(l):
-    out=[]
-    for c in l:
-        out1=[]
-        for c2 in c:
-            out2 = []
-            for c3 in c2:
-                map1 = map(stringify,c3)
-                result = list(map1)
-                out2.append(result)
-            out1.append(out2)
-        out.append(out1)
-    return out
-
+    def __call__(self, bytes_amount):
+        # To simplify, assume this is hooked up to a single filename
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            percentage = (self._seen_so_far / self._size) * 100
+            sys.stdout.write(
+                "\r%s  %s / %s  (%.2f%%)" % (
+                    self._filename, self._seen_so_far, self._size,
+                    percentage))
+            sys.stdout.flush()
+            
 def get_county_deaths(df_us, df_pops, start_date, end_date):
     fips_codes = df_us.fips.unique()
     df_pops['fips'] = df_pops.state_fips + df_pops.county_fips
@@ -59,71 +56,77 @@ def get_counties_features():
     with open('/home/anna_user2/datasets/geography/county-coords.json', 'r') as f:
         features = json.load(f)
     features = features['features']
-    features2 = []
+    states = {}
     for feature in features:
+        state_fips = feature['properties']['FIPS-code']
+        if state_fips not in states:
+            states[state_fips] = []
+    for feature in features:
+        state_fips = feature['properties']['FIPS-code']
         feature['properties']['name'] = feature['properties']['county'] + ' County'
-        # if feature['properties']['FIPS-code'] in ['01','02','04','05','06']:
-        #     features2.append(feature) 
-        features2.append(feature)
-    return features2
+        states[state_fips].append(feature)
+    return states
 
-def update_county_features(features, deaths):
-    for feature in features:
-        id = feature['properties']['FIPS-code'] +  feature['properties']['COUNTY']
-        feature['id'] = id
-        deaths1 = 0
-        if id in deaths.keys():
-            deaths1 = deaths[id]
-        feature['properties']['density'] = f'{deaths1}'
-    return features
+def update_county_features(states, deaths):
+    for state in states.keys():
+        for feature in states[state]:
+            id = feature['properties']['FIPS-code'] +  feature['properties']['COUNTY']
+            feature['id'] = id
+            deaths1 = 0
+            if id in deaths.keys():
+                deaths1 = deaths[id]
+            feature['properties']['density'] = f'{deaths1}'
+    return states
 
-table_name = 'counties_with_deaths'
-dynamodb=boto3.resource('dynamodb')
-table = dynamodb.Table(table_name)
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
 
-status, df_pops = county_pops_fips()
-if status is not None:
-    print(f'status from county_pops_fips: {status}')
-    exit()
-status, df_world = get_world_covid_jh()
-if status is not None:
-    print(f'status from get_world_covid_jh: {status}')
-    exit()
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
 
-df = df_world[df_world.index.get_level_values('ISO_A3')=='USA']
-df.reset_index(inplace=True)
-end_date = df.date.max()
-start_date = end_date-np.timedelta64(30,'D')
-print(f'date range {start_date} {end_date}')
-county_deaths = get_county_deaths(df, df_pops, start_date, end_date)
-features = get_counties_features()
-update_county_features(features, county_deaths)
-with open('temp.json', 'w') as f:
-    json.dump(features, f)
-#Convert numbers to strings to satisfy DynamoDB
-for feature in features:
-    coordinates = feature['geometry']['coordinates']
-    map_type = feature['geometry']['type']
-    if map_type == 'MultiPolygon':
-        coordinates = stringify_deep_multi_poly(feature['geometry']['coordinates'])
-    else:
-        coordinates = stringify_deep_poly(feature['geometry']['coordinates'])
-    feature['geometry']['coordinates'] = coordinates
-    feature['properties']['CENSUSAREA'] = stringify(feature['properties']['CENSUSAREA'])
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
 
-with open('../json/temp_county.json', 'w') as f:
-    json.dump(features, f)
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name, Callback=ProgressPercentage(path+state+'.json'))
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
-# for feature in features:
-#     fips = feature['id']
-#     print('writing dynamodb: ' + fips)
-#     # with open('temp.json','w') as f:
-#     #     json.dump(feature, f)
-#     response = table.put_item(
-#         Item = {
-#             'state_fips': fips[:2],
-#             'county_fips': fips[2:],
-#             'feature': feature,
-#             'supplemental': {}
-#         }
-#     )
+make_temp_file = True
+path = '/home/anna_user2/projects/website-II/json/'
+
+if make_temp_file:
+    status, df_pops = county_pops_fips()
+    if status is not None:
+        print(f'status from county_pops_fips: {status}')
+        exit()
+    status, df_world = get_world_covid_jh()
+    if status is not None:
+        print(f'status from get_world_covid_jh: {status}')
+        exit()
+
+    df = df_world[df_world.index.get_level_values('ISO_A3')=='USA']
+    df.reset_index(inplace=True)
+    end_date = df.date.max()
+    start_date = end_date-np.timedelta64(30,'D')
+    print(f'date range {start_date} {end_date}')
+    county_deaths = get_county_deaths(df, df_pops, start_date, end_date)
+    states = get_counties_features()
+    update_county_features(states, county_deaths)
+    for state in states.keys():
+        print(state)
+        with open(path + state + '.json', 'w') as f:
+            path2 = path + state + '.json'
+            feature_set = {'type': 'FeatureCollection', 'features': states[state]}
+            json.dump(feature_set,f)
+            f.flush()
+            upload_file(path2, 'phoenix-anna-web-content', state+'.json')
+        f.close()
