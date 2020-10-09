5721a350002d2ddbf79fe2d02e6ab0dc29124e99
Alan Turing <alan.turing@gmail.com>
2020-10-09 11:34:13 -0500

    fix overwritten county lambda function

--- py/county_lambda.py
+++ py/county_lambda.py
@@ -1,35 +1,35 @@
 import json
 import boto3
+import tempfile
+import os
+import ast
 
 def lambda_handler(event, context):
-    import boto3
 
-    table_name = 'state-features'
-    dynamodb=boto3.resource('dynamodb')
-    client = boto3.client('dynamodb')
-    table = dynamodb.Table(table_name)
-    
-    try :
-        response = table.scan()
-        items = response['Items']
+    params = event['queryStringParameters']
+    fips = params['fips']
+    try:
+        s3 = boto3.client('s3')
+        with tempfile.TemporaryDirectory() as tmpdir:
+            path = os.path.join(tmpdir, 'state_features.json')
+            s3.download_file('phoenix-technical-services.com', fips + '.json', path)
+            with open(path, 'r') as f:
+                obj = f.read()
     except Exception as inst:
         resp = {
             'statusCode': 404,
+            'headers': {
+            "Access-Control-Allow-Origin": "*"
+            },
             'body': f'{inst}'
         }
         return resp
-    
-    features = []
-    for item in items:
-        features.append(item['feature'])
-        
-    obj={'type': 'FeatureCollection', 'features': features}
 
     resp= {
         'statusCode': 200,
         'headers': {
             "Access-Control-Allow-Origin": "*"
         },
-        'body': json.dumps(obj)
+        'body': obj
         }
     return resp
