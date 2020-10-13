60c4997bb3cc8bb7e0eeb47aa23fabe1025771ac
Alan Turing <alan.turing@gmail.com>
2020-10-12 22:17:39 -0500

    line chart for us/county, untemplated

--- py/make_county_features.py
+++ py/make_county_features.py
@@ -12,6 +12,7 @@ import threading
 import numpy as np
 import json
 import pandas as pd
+import csv
 
 from county_pops_fips import county_pops_fips
 from get_world_covid_jh import get_world_covid_jh
@@ -36,6 +37,66 @@ class ProgressPercentage(object):
                     percentage))
             sys.stdout.flush()
             
+def get_cty_time_series(df, df_pops, fips, start_date, end_date):
+
+    # Select county and date range
+    df = df[['fips','date','deaths']]
+    df = df[df.date >= start_date]
+    df = df[df.date <= end_date]
+    df = df[df.fips==fips]
+    df = df[['date','deaths']]
+    
+    pop = df_pops[df_pops.fips == fips].population.iloc[0]
+    df.deaths *= 100000/pop
+
+    jsonstr = df.to_json(orient='records')
+
+    config = get_config()
+    path = config['FILES']['counties_states']
+
+    try:
+        df = pd.read_csv(path, dtype={'FIPS':str, 'county':str, 'state_abbr':str, 'state': str})
+    except Exception as inst:
+        return f'Exception reading {path} {inst})', None
+    df = df[df.FIPS == fips]
+    start_date = format(start_date,"%x")
+    end_date = format(end_date,"%x")
+    county_state = df.iloc[0].county + ' County, ' + df.iloc[0].state_abbr
+    jsonstr = '{' + f'"fips": {fips}, "start_date": "{start_date}", "end_date": "{end_date}", "county": "{county_state}", "stats":' + jsonstr + '}'
+
+    return None, jsonstr
+
+
+def get_us_time_series(df, start_date, end_date):
+
+    # Get population
+    config = get_config()
+    path = config['FILES']['world_census']
+    with open(path,"r") as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            if row[0] == 'United States':
+                pop = int(row[2])
+                break;
+
+    # Select county and date range
+    df = df[df.date >= start_date]
+    df = df[df.date <= end_date]
+    df = df[['date','deaths']]
+    df = df.groupby('date').deaths.sum().reset_index()
+    df.deaths *= 100000/pop
+    jsonstr = df.to_json(orient='records')
+
+    config = get_config()
+    path = config['FILES']['counties_states']
+
+    start_date = format(start_date,"%x")
+    end_date = format(end_date,"%x")
+    jsonstr = '{' + f'"start_date": "{start_date}", "end_date": "{end_date}", "stats":' + jsonstr + '}'
+
+    return None, jsonstr
+
+
 def world_populations():
     """
     Get a dictionary of world populations keyed by nation code
@@ -61,11 +122,6 @@ def world_populations():
 
 def get_county_deaths(df_us, df_pops, start_date, end_date):
     fips_codes = df_us.fips.unique()
-    df_pops['fips'] = df_pops.state_fips + df_pops.county_fips
-    df_pops.set_index(keys='fips', inplace=True)
-    df_pops.drop(columns=['state_fips', 'county_fips', 'state', 'county'], inplace=True)
-    pops_dict = df_pops.to_dict(orient='dict')
-    pops_dict = pops_dict['population']
     county_deaths = {}
     df1 = df_us[df_us.date == start_date]
     df2 = df_us[df_us.date == end_date]
@@ -180,6 +236,9 @@ def upload_file(file_name, bucket, object_name=None):
         return False
     return True
 
+# ----------------------------------------------------------
+# Main
+# ----------------------------------------------------------
 status, df_world = get_world_covid_jh()
 if status is not None:
     print(f'status from get_world_covid_jh: {status}')
@@ -188,7 +247,9 @@ if status is not None:
 df_world1 = df_world.reset_index()
 w_end_date = df_world1.date.max()
 w_start_date = w_end_date-np.timedelta64(30,'D')
-path = '/home/anna_user2/projects/website-II/json/'
+
+config = get_config()
+path = config['DEFAULT']['json']
 
 # Make and upload world features
 world_deaths = get_world_deaths(df_world1, w_start_date, w_end_date)
@@ -201,7 +262,7 @@ update_world_features(nations, world_deaths)
 
 #Upload
 interval = f'{w_start_date},{w_end_date}'
-path2 = path + '*.json'
+path2 = path + '/*.json'
 feature_list = []
 for key in nations.keys():
     f = nations[key][0]
@@ -229,15 +290,37 @@ if False:
             upload_file(path2, 'phoenix-technical-services.com', ISO_A3+'.json')
         f.close()
 
-# Make and upload county features
-status, df_pops = county_pops_fips()
-if status is not None:
-    print(f'status from county_pops_fips: {status}')
-    exit()
+# Get master data file
 df = df_world[df_world.index.get_level_values('ISO_A3')=='USA']
 df.reset_index(inplace=True)
 end_date = df.date.max()
 start_date = end_date-np.timedelta64(30,'D')
+start_date_graph = end_date-np.timedelta64(6,"M")
+
+# Make and upload county six-month stats
+status, df_pops = county_pops_fips()
+
+# Get population
+pops_dict = df_pops.to_dict(orient='dict')
+df_pops['fips'] = df_pops.state_fips + df_pops.county_fips
+# df_pops.set_index(keys='fips', inplace=True)
+df_pops.drop(columns=['state_fips', 'county_fips', 'state', 'county'], inplace=True)
+
+if status is not None:
+    print(f'status from county_pops_fips: {status}')
+    exit()
+
+status, county_time_series_json = get_cty_time_series(df, df_pops, '17031', start_date_graph, end_date)
+if status is not None:
+    print(f'{status} from get_cty_time_series')
+    exit(0)
+with open('temp.json', 'w') as f:
+    f.write(county_time_series_json)
+
+status, us_time_series_json = get_us_time_series(df, start_date_graph, end_date)
+with open('temp2.json', 'w') as f:
+    f.write(us_time_series_json)
+
 county_deaths = get_county_deaths(df, df_pops, start_date, end_date)
 states = get_counties_features()
 update_county_features(states, county_deaths)
@@ -245,8 +328,8 @@ update_county_features(states, county_deaths)
 #upload
 for state in states.keys():
     # print(state)
-    with open(path + state + '.json', 'w') as f:
-        path2 = path + state + '.json'
+    path2 = path + '/' + state + '.json'
+    with open(path2, 'w') as f:
         feature_set = {'type': 'FeatureCollection', 'features': states[state]}
         interval = f'{w_start_date},{w_end_date}'
         feature_obj = { 'interval': interval, 'feature_set': feature_set}
