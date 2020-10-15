f068fb25b72dd654339f0ee3b422173e956e6c69
Alan Turing <alan.turing@gmail.com>
2020-10-15 00:15:23 -0500

    before restructuring json for time series

--- py/county_lambda.py
+++ py/county_lambda.py
@@ -7,6 +7,10 @@ import ast
 def lambda_handler(event, context):
 
     params = event['queryStringParameters']
+    
+    #-----------------------------------
+    # World
+    #-----------------------------------
     if 'iso-a3' in params:
         ISO_A3 = params['iso-a3']
         try:
@@ -37,28 +41,127 @@ def lambda_handler(event, context):
         
     if 'fips' in params:
         fips = params['fips']
-        try:
-            s3 = boto3.client('s3')
-            with tempfile.TemporaryDirectory() as tmpdir:
-                path = os.path.join(tmpdir, 'state_features.json')
-                s3.download_file('phoenix-technical-services.com', fips + '.json', path)
-                with open(path, 'r') as f:
-                    obj = f.read()
-        except Exception as inst:
-            resp = {
-                'statusCode': 404,
-                'headers': {
-                "Access-Control-Allow-Origin": "*"
-                },
-                'body': f'{inst}'
-            }
-            return resp
+        
+        #-------------------------------------
+        # County map
+        #-------------------------------------
+        if len(fips) == 2:
+            try:
+                s3 = boto3.client('s3')
+                with tempfile.TemporaryDirectory() as tmpdir:
+                    path = os.path.join(tmpdir, 'scratch.json')
+                    s3.download_file('phoenix-technical-services.com', fips + '.json', path)
+                    with open(path, 'r') as f:
+                        obj = f.read()
+                    os.remove(path)
+            except Exception as inst:
+                resp = {
+                    'statusCode': 404,
+                    'headers': {
+                    "Access-Control-Allow-Origin": "*"
+                    },
+                    'body': f'{inst}'
+                }
+                return resp
     
-        resp= {
-            'statusCode': 200,
-            'headers': {
-                "Access-Control-Allow-Origin": "*"
-            },
-            'body': obj
-            }
-        return resp
+            resp= {
+                'statusCode': 200,
+                'headers': {
+                    "Access-Control-Allow-Origin": "*"
+                },
+                'body': obj
+                }
+            return resp
+
+    #--------------------------------------------
+    # County time series vs US
+    #-------------------------------------------
+        if len(fips) == 5:
+            try:
+                s3 = boto3.client('s3')
+                with tempfile.TemporaryDirectory() as tmpdir:
+                    path = os.path.join(tmpdir, 'scratch.json')
+                    s3.download_file('phoenix-technical-services.com', fips + '.json', path)
+                    with open(path, 'r') as f:
+                        county_ts = f.read()
+                    os.remove(path)
+            except Exception as inst:
+                resp = {
+                    'statusCode': 404,
+                    'headers': {
+                    "Access-Control-Allow-Origin": "*"
+                    },
+                    'body': f'{inst}'
+                }
+                return resp
+    
+            try:
+                s3 = boto3.client('s3')
+                with tempfile.TemporaryDirectory() as tmpdir:
+                    path = os.path.join(tmpdir, 'scratch.json')
+                    s3.download_file('phoenix-technical-services.com', 'us-time-series.json', path)
+                    with open(path, 'r') as f:
+                        us_ts = f.read()
+                    os.remove(path)
+            except Exception as inst:
+                resp = {
+                    'statusCode': 404,
+                    'headers': {
+                    "Access-Control-Allow-Origin": "*"
+                    },
+                    'body': f'{inst}'
+                }
+                return resp
+            us_county = '{ "us": ' + us_ts + ', "county": ' + county_ts + '}'
+            resp= {
+                'statusCode': 200,
+                'headers': {
+                    "Access-Control-Allow-Origin": "*"
+                },
+                'body': us_county
+                }
+            return resp
+
+            try:
+                s3 = boto3.client('s3')
+                with tempfile.TemporaryDirectory() as tmpdir:
+                    path = os.path.join(tmpdir, 'scratch.json')
+                    s3.download_file('phoenix-technical-services.com', fips + '.json', path)
+                    with open(path, 'r') as f:
+                        obj = f.read()
+                    os.remove(path)
+            except Exception as inst:
+                resp = {
+                    'statusCode': 404,
+                    'headers': {
+                    "Access-Control-Allow-Origin": "*"
+                    },
+                    'body': f'{inst}'
+                }
+                return resp
+    
+            try:
+                s3 = boto3.client('s3')
+                with tempfile.TemporaryDirectory() as tmpdir:
+                    path = os.path.join(tmpdir, 'scratch.json')
+                    s3.download_file('phoenix-technical-services.com', fips + '.json', path)
+                    with open(path, 'r') as f:
+                        us_ts = f.read()
+                    os.remove(path)
+            except Exception as inst:
+                resp = {
+                    'statusCode': 404,
+                    'headers': {
+                    "Access-Control-Allow-Origin": "*"
+                    },
+                    'body': f'{inst}'
+                }
+                return resp
+            resp= {
+                'statusCode': 200,
+                'headers': {
+                    "Access-Control-Allow-Origin": "*"
+                },
+                'body': obj
+                }
+            return resp
